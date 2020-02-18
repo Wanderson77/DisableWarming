a3a1d0e49b647db9f7eb5123320be0cf719b34fd
Wanderson Aldo <wanderson.aldo@gmail.com>
2020-02-18 09:09:03 -0300

    Initial commit

---
+++ src/components/disable.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { View } from "react-native";
+
+console.disableYellowBox = true;
+
+
+export default class App extends Component {
+
+  render() {
+    return (
+      <View>
+        {/*Your Code will be here*/}
+      </View>
+    );
+  }
+  
+}
